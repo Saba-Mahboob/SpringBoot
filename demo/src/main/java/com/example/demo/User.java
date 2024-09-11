6f7490f7e88b4714c0010831ca6fa5d11e595f0b
Saba-Mahboob <93098640+Saba-Mahboob@users.noreply.github.com>
2024-09-11 22:15:39 +0330

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String family;
+    private String email;
+    private String password;
+
+}
